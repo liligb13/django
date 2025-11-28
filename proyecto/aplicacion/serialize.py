2b9a68e8a74a519bfd1a19567b2ce42ff6fefbaa
liligb13 <liliangarcia379gmail.com>
2025-11-27 21:11:22 -0700

    prueba2

--- proyecto/aplicacion/serialize.py
+++ proyecto/aplicacion/serialize.py
@@ -7,6 +7,20 @@ class SensorSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class MedicionSerializer(serializers.ModelSerializer):
+    serial_sensor = serializers.CharField(write_only=True)
+    sensor = SensorSerializer(read_only=True) 
+
     class Meta:
         model = Medicion
-        fields = '__all__'  
+        fields = ('id', 'serial_sensor', 'sensor', 'valor', 'fecha_hora')
+        read_only_fields = ('fecha_hora', 'id')
+
+    def create(self, validated_data):
+        serial = validated_data.pop('serial_sensor')
+        
+        try:
+            sensor_obj = Sensor.objects.get(serial=serial)
+        except Sensor.DoesNotExist:
+            raise serializers.ValidationError({"serial_sensor": f"El sensor con serial '{serial}' no existe."})
+        medicion = Medicion.objects.create(sensor=sensor_obj, **validated_data)
+        return medicion
